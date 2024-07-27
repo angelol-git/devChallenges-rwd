6eb9c796617e256a1694a57b3aa45d69a370de28
aladores <angelo.ladores@outlook.com>
2024-07-27 17:38:57 -0400

    add support for click and drag, desktop only

--- js/mp/mp.js
+++ js/mp/mp.js
@@ -10,6 +10,8 @@ const songTimeCurrent = document.getElementById("song-time-current");
 const songTimeEnd = document.getElementById("song-time-end");
 //Input
 const songSeeker = document.getElementById("song-seeker");
+const progressBarContainer = document.getElementById("progress-bar-container");
+const progressBar = document.getElementById("progress-bar");
 const buttonPlayPause = document.getElementById("button-play-pause");
 const buttonNext = document.getElementById("button-next");
 const buttonPrev = document.getElementById("button-prev");
@@ -17,14 +19,30 @@ const buttonPrev = document.getElementById("button-prev");
 let songIndex = 0;
 let currentSong = document.createElement('audio');
 let isPlaying = false;
-let animationFrameId;
+let isDragging = false;
 
+currentSong.addEventListener("timeupdate", updateProgressBar);
+currentSong.addEventListener("ended", nextSong);
 buttonPlayPause.addEventListener("click", () => isPlaying ? pauseSong() : playSong());
 buttonPrev.addEventListener("click", prevSong);
 buttonNext.addEventListener("click", nextSong);
-songSeeker.addEventListener("input", handleSeekerInput);
-currentSong.addEventListener("timeupdate", updateSeeker);
-//currentSong.addEventListener("ended", nextSong);
+progressBarContainer.addEventListener("click", handleProgressBarClick);
+progressBarContainer.addEventListener('mousedown', (event) => {
+    if (event.target === progressBar) {
+        isDragging = true;
+        handleProgressBarDrag(event);
+    }
+});
+
+document.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+        handleProgressBarDrag(event);
+    }
+});
+
+document.addEventListener('mouseup', () => isDragging = false);
+// songSeeker.addEventListener("input", handleSeekerInput);
+// currentSong.addEventListener("timeupdate", updateSeeker);
 
 function playSong() {
     isPlaying = true;
@@ -36,7 +54,6 @@ function pauseSong() {
     isPlaying = false;
     buttonPlayPause.firstElementChild.src = "assets/Pause_fill.svg";
     currentSong.pause();
-    cancelAnimationFrame(animationFrameId);
 }
 
 function loadSong() {
@@ -45,12 +62,11 @@ function loadSong() {
     songTitle.innerText = songs[songIndex].title;
     artist.innerText = songs[songIndex].artist;
     songTimeCurrent.innerText = "0:00";
-    songSeeker.value = 0;
+    //songSeeker.value = 0;
 
-    //Wait for the file to finish loading to extract info
     currentSong.addEventListener('loadedmetadata', () => {
         songTimeEnd.innerText = formatSongTime(currentSong.duration);
-        songSeeker.max = Math.floor(currentSong.duration);
+        //songSeeker.max = Math.floor(currentSong.duration);
     });
 }
 
@@ -73,33 +89,37 @@ function nextSong() {
     playSong();
 }
 
-function updateSeeker() {
+function updateProgressBar(event) {
+    const { currentTime, duration } = event.srcElement;
+    const progressBarPercent = (currentTime / duration) * 100;
+
+    progressBar.style.width = `${progressBarPercent}%`;
 
     let currentMinutes = Math.floor(currentSong.currentTime / 60);
     let currentSeconds = Math.floor(currentSong.currentTime % 60);
     if (currentSeconds < 10) {
         currentSeconds = "0" + currentSeconds;
     }
-
-    const completionPercentage = Math.floor((currentSong.currentTime / currentSong.duration) * 100);
-    if (isNaN(completionPercentage)) {
-        songSeeker.style.background = `linear-gradient(to right, var(--accent) 0%, var(--accent) 1%, var(--text) 1%, var(--text) 100%)`;
-    }
-    else {
-        songSeeker.style.background = `linear-gradient(to right, var(--accent) 0%, var(--accent) ${completionPercentage}%, var(--text) ${completionPercentage}%, var(--text) 100%)`;
-    }
-    songSeeker.value = Math.floor(currentSong.currentTime);
     songTimeCurrent.innerText = `${currentMinutes}:${currentSeconds}`;
-
-    animationFrameId = requestAnimationFrame(updateSeeker);
 }
 
-function handleSeekerInput(event) {
-    const seekerInputValue = event.target.value;
-    const formattedString = formatSongTime(seekerInputValue);
-    songTimeCurrent.innerText = formattedString;
-    currentSong.currentTime = seekerInputValue;
-    updateSeeker();
+function handleProgressBarClick(event) {
+    const width = this.clientWidth;
+    const clickedX = event.offsetX;
+    const duration = currentSong.duration;
+    // console.log(currentSong.duration);
+    // console.log((clickedX / width) * duration);
+    currentSong.currentTime = (clickedX / width) * duration;
+}
+
+function handleProgressBarDrag(event) {
+    const rect = progressBarContainer.getBoundingClientRect();
+    let newX = event.clientX - rect.left;
+    newX = Math.max(0, Math.min(newX, rect.width));
+    const duration = currentSong.duration;
+    const newTime = (newX / rect.width) * duration;
+    currentSong.currentTime = newTime;
+    updateProgressBar(event);
 }
 
 function formatSongTime(seconds) {
@@ -112,3 +132,32 @@ function formatSongTime(seconds) {
 }
 
 loadSong();
+
+// function updateSeeker() {
+
+//     let currentMinutes = Math.floor(currentSong.currentTime / 60);
+//     let currentSeconds = Math.floor(currentSong.currentTime % 60);
+//     if (currentSeconds < 10) {
+//         currentSeconds = "0" + currentSeconds;
+//     }
+
+//     const completionPercentage = Math.floor((currentSong.currentTime / currentSong.duration) * 100);
+//     if (isNaN(completionPercentage)) {
+//         songSeeker.style.background = `linear-gradient(to right, var(--accent) 0%, var(--accent) 1%, var(--text) 1%, var(--text) 100%)`;
+//     }
+//     else {
+//         songSeeker.style.background = `linear-gradient(to right, var(--accent) 0%, var(--accent) ${completionPercentage}%, var(--text) ${completionPercentage}%, var(--text) 100%)`;
+//     }
+//     songSeeker.value = Math.floor(currentSong.currentTime);
+//     songTimeCurrent.innerText = `${currentMinutes}:${currentSeconds}`;
+
+// }
+
+// function handleSeekerInput(event) {
+//     const seekerInputValue = event.target.value;
+//     const formattedString = formatSongTime(seekerInputValue);
+//     songTimeCurrent.innerText = formattedString;
+//     currentSong.currentTime = seekerInputValue;
+//     updateSeeker();
+// }
+
